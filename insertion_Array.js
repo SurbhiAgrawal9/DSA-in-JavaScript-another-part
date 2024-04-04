c8fb6872a4552a84172fdb04864d933fb65a25c9
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2024-04-05 01:02:27 +0530

    Create insertion_Array.js

---
+++ insertion_Array.js
@@ -0,0 +1,13 @@
+//element insert in Array
+let data = [60, 30, 10, 67, 40];
+        let newElement = 50;
+        let possition = 2;
+        console.log(data)
+        for (let i = data.length - 1; i >= possition; i--) {
+            data[i + 1] = data[i]
+            if (i == possition) {
+                data[possition] = newElement;
+            }
+
+        }
+        console.log(data)
