384ac7b94eeab69d66911143e1828a26df463fdf
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2024-04-05 01:04:01 +0530

    Update insertion_Array.js

--- insertion_Array.js
+++ insertion_Array.js
@@ -6,7 +6,8 @@ let data = [60, 30, 10, 67, 40];
         for (let i = data.length - 1; i >= possition; i--) {
             data[i + 1] = data[i]
             if (i == possition) {
-                data[possition] = newElement;
+               // data[possition] = newElement  
+                    data[i] = newElement;
             }
 
         }
