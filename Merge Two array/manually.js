9321cc19c93a829c21f15edd686f15cc1d5adbac
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2024-05-05 00:12:05 +0530

    Update manually.js

--- Merge Two array/manually.js
+++ Merge Two array/manually.js
@@ -1,14 +1,14 @@
 let data = [2,3,60,48,20];
-let data2 = [40,21,45,60];
+let data2 = [40,21,45,60,6];
 let data3 = [];
 
-for(i=0;i<data.length;i++){
+for(let i=0;i<data.length;i++){
     data3[i] = data[i];
     
 }
 
-for(i=0;i<data2.length;i++){
-    data3[data.length] = data2[i];
-    console.log(data.length)
+for(let i=0;i<data2.length;i++){
+    data3[data.length + i] = data2[i];
+    console.log(data.length + i)
 }
 console.log(data3)
